4ef136ed53722b968c1daf14db7050abfb6489ae
Papun1111 <gohanmohapatra@gmail.com>
2024-08-13 12:51:16 +0530

    frontend updated

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import LandingPage from './pages/landing';
+import Authentication from './pages/authetication';
+import { AuthProvider } from './contexts/AuthContext';
+import VideoMeetComponent from './pages/VideoMeet';
+import HomeComponent from './pages/Home';
+import History from './pages/history';
+function App() {
+  return (
+<>
+<Router>
+  <AuthProvider>  <Routes>
+    {/* <Route path="/home" element=""></Route> */}
+    <Route path="/" element={<LandingPage></LandingPage>}></Route>
+    <Route path='/history' element={<History></History>}></Route>
+    <Route path="/home" element={<HomeComponent></HomeComponent>}></Route>
+    <Route path="/auth" element={<Authentication></Authentication>}></Route>
+    <Route path="/:url" element={<VideoMeetComponent></VideoMeetComponent>}></Route>
+  </Routes>
+  </AuthProvider>
+
+</Router>
+</>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/landing.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "../App.css";
+import {Link, useNavigate} from 'react-router-dom';
+export default function LandingPage() {
+  const router=useNavigate();
+  return (
+    <div className="landingPageContainer">
+      <nav>
+        <div className="navHeader">
+          <h2>Gusion Dagger Calls</h2>
+        </div>
+        <div className="navlist">
+          <p onClick={()=>{
+           router("/asdad");
+          }}>Join as Guest</p>
+          <p onClick={()=>{
+            router("/auth");
+          }}>Register</p>
+          <p onClick={()=>{
+            router("/home");
+          }}>Home</p>
+          <div role="button">
+            <p onClick={()=>{
+              router("/auth");
+            }}>Log In</p>
+          </div>
+        </div>
+      </nav>
+
+      <div className="landingMainContainer">
+        <div>
+          <h1>
+            <span style={{ color: "#DA70D6" }}>Welcome</span> to Gusion Dagger
+            Calls
+          </h1>
+          <p>Cover a distance by Gusion dagger calls</p>
+          <div role="button">
+            <Link to={"/auth"}>Get Started</Link>
+          </div>
+        </div>
+
+        <div className="imgDisp">
+          <img src="/videocall.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
